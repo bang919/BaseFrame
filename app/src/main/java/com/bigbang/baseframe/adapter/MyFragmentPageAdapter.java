0e1697d78de5151257bcd3d8548844b92c5f4aa7
bigbang <rongbangou@gmail.com>
2018-09-08 18:48:33 +0800

    把对返回键的处理加入了MyFragmentPageAdapter

--- app/src/main/java/com/bigbang/baseframe/adapter/MyFragmentPageAdapter.java
+++ app/src/main/java/com/bigbang/baseframe/adapter/MyFragmentPageAdapter.java
@@ -1,10 +1,16 @@
 package com.bigbang.baseframe.adapter;
 
+import android.arch.lifecycle.Lifecycle;
+import android.arch.lifecycle.LifecycleObserver;
+import android.arch.lifecycle.LifecycleOwner;
+import android.arch.lifecycle.OnLifecycleEvent;
 import android.support.annotation.IdRes;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.view.KeyEvent;
+import android.view.View;
 
 import com.bigbang.baseframe.R;
 
@@ -22,11 +28,49 @@ public class MyFragmentPageAdapter {
     public MyFragmentPageAdapter(FragmentActivity fragmentActivity, @IdRes int containerViewId) {
         mFragmentActivity = fragmentActivity;
         this.containerViewId = containerViewId;
+        mFragmentActivity.getLifecycle().addObserver(new LifecycleObserver() {
+            @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+            void onCreate(LifecycleOwner lifecycleOwner) {
+                View decorView = mFragmentActivity.getWindow().getDecorView();
+                decorView.setFocusableInTouchMode(true);
+                decorView.requestFocus();
+                decorView.setOnKeyListener(new View.OnKeyListener() {
+                    @Override
+                    public boolean onKey(View v, int keyCode, KeyEvent event) {
+                        if ((keyCode == KeyEvent.KEYCODE_BACK || keyCode == KeyEvent.KEYCODE_ESCAPE) && event.getAction() == KeyEvent.ACTION_DOWN) {
+                            return mFragmentActivity.getSupportFragmentManager().popBackStackImmediate();
+                        }
+                        return false;
+                    }
+                });
+                mFragmentActivity.getLifecycle().removeObserver(this);
+            }
+        });
     }
 
     public MyFragmentPageAdapter(Fragment fragment, @IdRes int containerViewId) {
         mFragment = fragment;
         this.containerViewId = containerViewId;
+        mFragment.getLifecycle().addObserver(new LifecycleObserver() {
+            @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+            void onResume(LifecycleOwner lifecycleOwner) {
+                //需要Resume后才能获得getView
+                if (mFragment.getView() != null) {
+                    mFragment.getView().setFocusableInTouchMode(true);
+                    mFragment.getView().requestFocus();
+                    mFragment.getView().setOnKeyListener(new View.OnKeyListener() {
+                        @Override
+                        public boolean onKey(View v, int keyCode, KeyEvent event) {
+                            if ((keyCode == KeyEvent.KEYCODE_BACK || keyCode == KeyEvent.KEYCODE_ESCAPE) && event.getAction() == KeyEvent.ACTION_DOWN) {
+                                return mFragment.getChildFragmentManager().popBackStackImmediate();
+                            }
+                            return false;
+                        }
+                    });
+                }
+                mFragment.getLifecycle().removeObserver(this);
+            }
+        });
     }
 
     public void switchToFragment(Fragment targetFragment) {
@@ -60,7 +104,7 @@ public class MyFragmentPageAdapter {
             currentFragment = targetFragment;
         } else {
             transaction.replace(containerViewId, targetFragment, targetFragment.getClass().getName());
-            transaction.addToBackStack(null);
+            transaction.addToBackStack(targetFragment.getClass().getName());
             transaction.commitAllowingStateLoss();
         }
     }
